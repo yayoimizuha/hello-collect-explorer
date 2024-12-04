cd0315a0e63c7fe36e7738b26cf226550c51a669
yayoimizuha <poko.peko.tomo@icloud.com>
2024-12-05 02:23:05 +0900

    update

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -4,27 +4,54 @@ use std::fs;
 use reqwest;
 use serde_json::Value;
 
-static SECURE_TOKEN: OnceCell<String> = OnceCell::new();
 
-async fn generate_secure_token(login_id: HashMap<String, String>) -> Value {
-    let client = reqwest::Client::new();
-    let custom_token = client.post("https://account-api.orical.jp/firebase_user/generate_custom_token?idprovider_key=helloproject_id").
-        json(&login_id).send().await.unwrap().json::<Value>().await.unwrap()
-        ["custom_token"].as_str().unwrap().to_string();
-    client.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyABJQ_1lLpugYT2kuzdCsRmcx0P8QRG16s").
-        json(&HashMap::from([("token", custom_token), ("returnSecureToken", "true".to_string())])).send().await.unwrap().json::<Value>().await.unwrap()
+#[derive(Debug)]
+struct OricalContainer {
+    custom_token: String,
+    secure_token: String,
+    user_id: i64,
+    orical_id: i64,
+    screen_name: String,
 }
+impl OricalContainer {
+    async fn get_secure_token(custom_token: String) -> String {
+        let client = reqwest::Client::new();
+        client.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyABJQ_1lLpugYT2kuzdCsRmcx0P8QRG16s").
+            json(&HashMap::from([("token", custom_token), ("returnSecureToken", "true".to_string())])).send().await.unwrap().json::<Value>().await.unwrap()["idToken"].as_str().unwrap().to_string()
+    }
+    async fn get_custom_token(login_id: HashMap<String, String>) -> String {
+        let client = reqwest::Client::new();
+        client.post("https://account-api.orical.jp/firebase_user/generate_custom_token?idprovider_key=helloproject_id").
+            json(&login_id).send().await.unwrap().json::<Value>().await.unwrap()["custom_token"].as_str().unwrap().to_string()
+    }
+    async fn new(login_id: HashMap<String, String>) -> OricalContainer {
+        let client = reqwest::Client::new();
+        let custom_token = OricalContainer::get_custom_token(login_id).await;
+        let secure_token = OricalContainer::get_secure_token(custom_token.clone()).await;
+        let user_desc = client.get("https://api-helloproject.orical.jp/partner_users?partner_id=13")
+            .header(reqwest::header::AUTHORIZATION.to_string(), secure_token.clone()).send().await.unwrap().json::<Value>().await.unwrap();
+        let user_id = user_desc["user_id"].as_i64().unwrap();
+        let screen_name = user_desc["screen_name"].as_str().unwrap().to_string();
+        let orical_id = user_desc["orica"]["id"].as_i64().unwrap();
+
+        OricalContainer { custom_token, secure_token, user_id, orical_id, screen_name }
+    }
+
+    // async fn has_bonus(&self) -> Value {
+    //     let client = reqwest::Client::new();
+    //     client.put("https://api-helloproject.orical.jp/loginbonuses/check?partner_id=13&user_id")
+    //         .header(reqwest::header::AUTHORIZATION.to_string(), self.secure_token.clone(),
+    //         ).send().await.unwrap().json::<Value>().await.unwrap()
+    // }
+}
+// https://api-helloproject.orical.jp/card_users/count_by_stars?partner_id=13&screen_name=yayoi_mizuha
 #[tokio::main]
 async fn main() {
     let login_id = serde_json::from_str::<Value>(fs::read_to_string("login_info.json").unwrap().as_str()).unwrap().as_object().unwrap().iter().map(|(k, v)| {
         (k.clone(), v.clone().as_str().unwrap().to_string())
     }).collect::<HashMap<String, String>>();
 
-    let client = reqwest::Client::new();
-    SECURE_TOKEN.set(generate_secure_token(login_id).await["idToken"].as_str().unwrap().to_string()).unwrap();
-    println!("{}", SECURE_TOKEN.get().unwrap());
+    let orical_container = OricalContainer::new(login_id).await;
 
-    println!("{}", client.get("https://api-helloproject.orical.jp/presentboxes/check_if_unreceived_exists?context=actionbonus&partner_id=13")
-        .header(reqwest::header::AUTHORIZATION.to_string(), SECURE_TOKEN.get().unwrap()
-        ).send().await.unwrap().text().await.unwrap());
+    // println!("has bonus?: {}", orical_container.has_bonus().await);
 }
